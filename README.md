b1dee17671491f0ed9a4f2ddc74ff148fd01b24b
lisnychyis <serhii.lisnychyi@onedome.com>
2021-05-12 16:10:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+###### JDD May,2021
+kovalevskyi course source code
+
+modules: [#1 i/o, ...]
---
+++ jdd-module1-io/src/test/java/academy/kovalevskyi/javadeepdive/week0/day0/StdBufferedReaderTest.java
@@ -0,0 +1,35 @@
+package academy.kovalevskyi.javadeepdive.week0.day0;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+class StdBufferedReaderTest {
+
+    private static Stream<Arguments> args() {
+        return Stream.of(
+                Arguments.of("hello \n world", new String[]{"hello ", " world"}),
+                Arguments.of("hello \r world", new String[]{"hello ", " world"}),
+                Arguments.of("hello \n\r world", new String[]{"hello ", "", " world"}),
+                Arguments.of("hellosdfgaw\n\r world", new String[]{"hellosdfgaw", "", " world"}),
+                Arguments.of("not blank", new String[]{"not blank"})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("args")
+    public void should_readLine_whenLineExits(String input, String[] expected) throws IOException {
+        StdBufferedReader reader = new StdBufferedReader(
+                new InputStreamReader(new ByteArrayInputStream(input.getBytes())));
+        for (String s : expected) {
+            char[] result = reader.readLine();
+            Assertions.assertThat(result).isEqualTo(s.toCharArray());
+        }
+    }
+}
